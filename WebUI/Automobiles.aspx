3571525be4073e8386eb768e36760c8589651768
Cardi DeMonaco Jr <cardi@rokpte.com>
2018-10-12 16:02:20 -0400

    Added a WebUI project
    Planning on making a user interface for this data...

---
+++ WebUI/Automobiles.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master.Master" AutoEventWireup="true" CodeBehind="Automobiles.aspx.cs" Inherits="WebUI.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+        Make:
+        <asp:DropDownList ID="ddlMake" runat="server">
+        </asp:DropDownList>
+    </p>
+    <p>
+        Model:
+        <asp:DropDownList ID="ddlModel" runat="server">
+        </asp:DropDownList>
+    </p>
+    <p>
+        <asp:GridView ID="gvAutomobiles" runat="server">
+        </asp:GridView>
+    </p>
+</asp:Content>
---
+++ WebUI/Automobiles.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Cars;
+
+namespace WebUI
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //Create Cars database...
+                Database.SetInitializer(new DropCreateDatabaseIfModelChanges<CarDb>());
+                InsertData();
+
+                //Create access to Cars database...
+                var db = new CarDb();
+                //db.Database.Log = Console.WriteLine;
+
+                //Populate Make drop-down...
+
+
+
+                //Populate Model drop-down...
+
+
+
+                //Populate Automobiles Grid View...
+                var query =
+                    from car in db.Cars
+                    group car by car.Manufacturer into manufacturer
+                    select new
+                    {
+                        Name = manufacturer.Key,
+                        Cars = (from car in manufacturer
+                                orderby car.Combined descending
+                                select car).Take(2)
+                    };
+
+                gvAutomobiles.DataSource = query.ToList();
+                gvAutomobiles.DataBind();
+            }
+        }
+
+        public static void InsertData()
+        {
+            var cars = ProcessCars(@"E:\GitHub\linqsamples\WebUI\Data\fuel.csv");
+            var db = new CarDb();
+
+            if (!db.Cars.Any())
+            {
+                foreach (var car in cars)
+                {
+                    db.Cars.Add(car);
+                }
+                db.SaveChanges();
+            }
+        }
+
+        private static List<Car> ProcessCars(string path)
+        {
+            var query =
+
+                File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(l => l.Length > 1)
+                    .ToCar();
+
+            return query.ToList();
+        }
+    }
+}
